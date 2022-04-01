b5e11643ac5512e54454a90b5309fa0fc0e26a47
nefremov <Nikita.Efremov@t-systems.com>
2022-04-01 11:22:42 +0300

    Add gradle project

---
+++ src/main/java/nikitaefremov/newalbumfetcher/NewAlbumFetcherApplication.java
@@ -0,0 +1,13 @@
+package nikitaefremov.newalbumfetcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewAlbumFetcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewAlbumFetcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nikitaefremov/newalbumfetcher/NewAlbumFetcherApplicationTests.java
@@ -0,0 +1,13 @@
+package nikitaefremov.newalbumfetcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewAlbumFetcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
